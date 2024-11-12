f00b00f444cfe7a9ddf955f133d332e30d7a458a
SivanRP <sivanreddypushpagiri@gmail.com>
2024-11-11 20:22:03 -0500

    modded

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Add event listener for the hamburger icon
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  hamburgerIcon.addEventListener("click", toggleMenu);
+});
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
