38a07ff4659502857a48bbe0a74da0bb25170a02
SivanRP <sivanreddypushpagiri@gmail.com>
2024-11-11 20:40:02 -0500

    again

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
